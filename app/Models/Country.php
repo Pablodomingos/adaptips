fb1e7f992f16e0a8920ee64277899caac203dddd
Pablo Domingos <pablodomingos1700@gmail.com>
2021-09-05 22:21:10 -0300

    Meu terceiro commit

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Movie extends Model
+class Country extends Model
 {
     use HasFactory;
-    public function country(){
-        return $this->belongsTo('App\Models\Country');
+    public function movie(){
+        return $this->hasOne('App\Models\Movie');
     }
 }
